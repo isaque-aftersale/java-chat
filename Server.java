50f02d4a575efe5349388a46b749e309c62901de
isaque.aftersale <isaque.santos@after.sale>
2024-04-16 19:50:37 -0300

    fix

--- Server.java
+++ Server.java
@@ -1,13 +1,11 @@
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketOption;
-import java.net.SocketOptions;
 import java.util.Scanner;
 
 public class Server {
     private static ServerSocket server;
+    private static Socket client;
     private static Scanner inputScanner;
     private static final Integer PORT = 3000;
 
@@ -16,10 +14,11 @@ public class Server {
         try {
             server = new ServerSocket(PORT);
 
-            server.setReuseAddress(true);
+            client = server.accept();
+            inputScanner = new Scanner(client.getInputStream());
 
             do {
-                handleClient(server);
+                handleClient();
             } while (true);
 
         } catch (IOException e) {
@@ -28,21 +27,11 @@ public class Server {
 
     }
 
-    private static void handleClient(ServerSocket server) {
-        try {
-            Socket client = server.accept();
+    private static void handleClient() {
+        while (!inputScanner.hasNextLine())
+            return;
 
-            inputScanner = new Scanner(client.getInputStream());
+        System.out.println("client-" + client.getInetAddress().getHostName() + ": " + inputScanner.nextLine());
 
-            while (inputScanner.hasNextLine()) {
-                System.out.println("client-" + client.getInetAddress().getHostName() + ": " + inputScanner.nextLine());
-
-                if (!client.isBound()) {
-                    client.bind(new InetSocketAddress(server.getInetAddress(), 3000));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
